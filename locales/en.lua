471cea28f528894a72c8dbbae33e2434f3c4fc69
Valerio <valerio.cavagni@gmail.com>
2021-04-14 23:13:04 +0200

    [Add] Update locale for general usage.

--- locales/en.lua
+++ locales/en.lua
@@ -1,4 +1,6 @@
 Locales['en'] = {
   ['used_bread'] = 'you have used ~y~1x~s~ ~b~bread~s~',
   ['used_water'] = 'you have used ~y~1x~s~ ~b~water~s~',
+  ['used_thirst'] = 'you have ~b~drinked~s~',
+  ['used_hunger'] = 'you have ~b~eaten~s~',
 }
