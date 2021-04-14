bf75795cc8aa2a6bad6df50d4153606ba8e729a5
Valerio <valerio.cavagni@gmail.com>
2021-04-14 23:14:16 +0200

    [Upd] Add default items to config

--- config.lua
+++ config.lua
@@ -1,3 +1,33 @@
 Config = {}
 Config.Locale = 'en'
 Config.Visible = true
+Config.Items = {
+	{
+		name = 'bread',
+		pType = 'hunger',
+		value = '200000'
+	},
+	{
+		name = 'water',
+		pType = 'thirst',
+		value = '200000'
+	},
+	-- Example --
+	--[[ 
+	{
+		name = 'icetea',
+		pType = 'thirst',
+		value = '200000'
+	},
+	{
+		name = 'beer',
+		pType = 'thirst',
+		value = '200000'
+	},
+	{
+		name = 'coffe',
+		pType = 'thirst',
+		value = '50000'
+	}
+	]]--
+}
